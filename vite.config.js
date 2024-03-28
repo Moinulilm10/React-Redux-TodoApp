bd917d79f8761d61753f89e33af812dce44543c8
Moinul Islam <moinulilm10@gmail.com>
2024-03-29 00:11:51 +0600

    v-2.0 branch created and code transfer main to v-2.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Redux-TodoApp/",
+  // base: "/React-Redux-TodoApp/",
   plugins: [react()],
 });
